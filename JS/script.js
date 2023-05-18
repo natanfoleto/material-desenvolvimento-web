20f902b93322734f393b07346edf96f1ddb5ca76
Natan Foleto <natanfoleto2015@hotmail.com>
2023-05-18 18:10:55 -0300

    Add excercicios

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// Exercícios de funções
+
+// 1. Crie uma função que divida dois números e retorne o resultado
+
+// 2. Crie uma função que converte temperatura de Celsius para Fahrenheit
+// Fórmula = (celsius * 9/5) + 32;
+
+// 3. Crie uma função para calcular o quadrado de um número
+
+// 4. Crie uma função para calcular a quantidade que irei gastar ao abastecer um carro
+// Entradas: preço do combustível, quantidade de litros
